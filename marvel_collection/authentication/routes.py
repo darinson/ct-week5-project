d2f18ba3402c7e3eeba1041c6dbc7c33dccfc8aa
Darin Son <dson64@outlook.com>
2021-04-16 08:44:44 -0700

    added authentication messages to signinsignupindex

--- marvel_collection/authentication/routes.py
+++ marvel_collection/authentication/routes.py
@@ -18,7 +18,7 @@ def signup():
             db.session.add(user)
             db.session.commit()
 
-            flash(f'You have successfully created a user account: {email}', 'user-created')
+            flash(f'Welcome, {name}.\nYou have successfully created a user account: {email}', 'user-created')
 
             return redirect(url_for('site.home'))
     except:
@@ -28,7 +28,29 @@ def signup():
 
 @auth.route('/signin',methods=['GET','POST'])
 def signin():
-    return render_template('signin.html')
+    form = UserLoginForm()
+
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+            print(email,password)
+
+            logged_user = User.query.filter(User.email == email).first() 
+
+            if logged_user and check_password_hash(logged_user.password, password):
+                login_user(logged_user)
+                flash(f'Welcome back, {logged_user.name}! You were successfully logged in.', 'auth-sucess')
+                return redirect(url_for('site.index'))
+            else:
+                flash('Your email/password is incorrect', 'auth-failed')
+
+                return redirect(url_for('auth.signin'))
+
+    except:
+        raise Exception('Invalid Form Data: Please Check Your Form')
+
+    return render_template('signin.html', form = form)
 
 # @auth.route('/logout')
 # # @login_required #need to import
